c0a8619a9a1b38e31cc2dba5676f8aa74fca1f98
Juan Puerto <jpuerto96@hotmail.com>
2021-11-10 16:15:50 -0500

    Views: Add basic logic for GET and POST

--- user_workspaces_server/views.py
+++ user_workspaces_server/views.py
@@ -3,16 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from django.views import View
+from . import models
+import json
 
 
 class WorkspaceView(View):
-    def get(self, request, workspace_uuid=None):
-        return JsonResponse({'message': 'Successful!', 'success': True})
+    def get(self, request, workspace_id=None):
+        workspace = models.Workspace.objects.filter(id=workspace_id).first()
+
+        # TODO: Add url parameter searching functionality
+
+        return JsonResponse({'message': 'Successful!', 'success': True, 'data': {'workspace': workspace}})
 
     def post(self, request):
+        body = json.loads(request.body)
+
+        workspace_data = {}
+
+        workspace = models.Workspace(**workspace_data)
+        workspace.save()
+
         return JsonResponse({'message': 'Successful!', 'success': True})
 
-    def put(self, request, workspace_uuid, put_type):
+    def put(self, request, workspace_id, put_type):
         if put_type.lower() == 'start':
             return JsonResponse({'message': 'Successful start!', 'success': True})
         else:
@@ -20,10 +33,14 @@ class WorkspaceView(View):
 
 
 class JobView(View):
-    def get(self, request, job_uuid=None):
-        return JsonResponse({'message': 'Successful!', 'success': True})
+    def get(self, request, job_id=None):
+        job = models.Job.objects.filter(id=job_id).first()
 
-    def put(self, request, job_uuid, put_type):
+        # TODO: Add url parameter searching functionality
+
+        return JsonResponse({'message': 'Successful!', 'success': True, 'data': {'job': job}})
+
+    def put(self, request, job_id, put_type):
         if put_type.lower() == 'stop':
             return JsonResponse({'message': 'Successful stop!', 'success': True})
         else:
@@ -32,4 +49,6 @@ class JobView(View):
 
 class JobTypeView(View):
     def get(self, request):
-        return JsonResponse({'message': 'Successful!', 'success': True})
+        # TODO: Grab job types from the config.
+        job_types = {}
+        return JsonResponse({'message': 'Successful!', 'success': True, 'data': {'job_types': job_types}})
