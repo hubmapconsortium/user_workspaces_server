0c72c5f52d9dae4b51244dfc7aeb69aaeba0c0a4
Juan Puerto <jpuerto96@hotmail.com>
2021-12-20 13:22:25 -0500

    Views: Add functionality to allow API users to grab user based tokens.

--- user_workspaces_server/views.py
+++ user_workspaces_server/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.conf import settings
 from django.http import JsonResponse
 from django.views import View
+from django.contrib.auth.models import User
 from . import models
+from .controllers.userauthenticationmethods import globus_user_authentication
 import json
 from datetime import datetime
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class UserWorkspacesServerTokenView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        # grab the main auth method, just force globus auth for now
+        user_auth = globus_user_authentication.GlobusUserAuthentication(
+            settings.CONFIG['available_user_authentication'][settings.CONFIG['api_user_authentication']]
+            ['connection_details']
+        )
+
+        # hit the api_authenticate method
+        api_user = user_auth.api_authenticate(request)
+
+        if type(api_user) == User:
+            token, created = Token.objects.get_or_create(user=api_user)
+            result = Response({'token': token.key})
+        elif type(api_user) == Response:
+            result = api_user
+        else:
+            raise AuthenticationFailed
+
+        return result
 
 
 class WorkspaceView(View):
