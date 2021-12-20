4118b933fc3574502f1537e777b289e10b86c9ce
Juan Puerto <jpuerto96@hotmail.com>
2021-12-20 14:13:54 -0500

    Apps: Add initilization code.

--- user_workspaces_server/apps.py
+++ user_workspaces_server/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UserWorkspacesServerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_workspaces_server'
+
+    def ready(self):
+        # TODO: Initialize all of the resources here
+        pass
