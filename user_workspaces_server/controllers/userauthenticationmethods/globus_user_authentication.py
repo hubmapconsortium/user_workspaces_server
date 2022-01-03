51146f46bfbb24fee44f5ef8eaba19c8ff10848c
Juan Puerto <jpuerto96@hotmail.com>
2022-01-03 11:39:33 -0500

    User Auth: Small changes to globus user auth

--- user_workspaces_server/controllers/userauthenticationmethods/globus_user_authentication.py
+++ user_workspaces_server/controllers/userauthenticationmethods/globus_user_authentication.py
@@ -1,9 +1,7 @@
 from user_workspaces_server.controllers.userauthenticationmethods.abstract_user_authentication import \
     AbstractUserAuthentication
-from user_workspaces_server import models
 from rest_framework.response import Response
 from rest_framework.exceptions import APIException
-from django.contrib.auth.models import User
 import globus_sdk
 import json
 from hubmap_commons.hm_auth import AuthHelper
@@ -19,6 +17,9 @@ class GlobusUserAuthentication(AbstractUserAuthentication):
             self.auth_helper = AuthHelper.create(clientId=client_id, clientSecret=client_secret)
         else:
             self.auth_helper = AuthHelper.instance()
+
+    def has_permission(self, internal_user):
+        pass
 
     def api_authenticate(self, request):
         body = json.loads(request.body)
@@ -45,53 +46,54 @@ class GlobusUserAuthentication(AbstractUserAuthentication):
                 user_info = self.introspect_globus_user(
                     tokens.by_resource_server['groups.api.globus.org']['access_token'])
 
-                external_user = models.ExternalUserMapping.objects.filter(
-                    external_user_id=user_info['sub'], user_authentication_name=type(self).__name__
-                ).first()
+                external_user_mapping = self.get_external_user_mapping({
+                    'external_user_id': user_info['sub'],
+                    'user_authentication_name': type(self).__name__
+                })
 
-                return self.create_external_user_mapping(user_info).user_id \
-                    if not external_user else external_user.user_id
+                if not external_user_mapping:
+                    # Since its Globus, lets get the username from the email
+                    username = user_info['email'].split('@')[0]
+                    internal_user = self.get_internal_user({
+                        'username': username,
+                        'email': user_info['email']
+                    })
+
+                    if not internal_user:
+                        internal_user = self.create_internal_user({
+                            "username": username,
+                            "email": user_info['email']
+                        })
+
+                    user_info['internal_user_id'] = internal_user
+                    self.create_external_user_mapping({
+                        'user_id': user_info['internal_user_id'],
+                        'user_authentication_name': type(self).__name__,
+                        'external_user_id': user_info['sub'],
+                        'external_username': user_info['username']
+                    })
+                    return internal_user
+                else:
+                    return external_user_mapping.user_id
         except Exception as e:
+            # TODO: Move print to log
+            print(e)
             return e
-
-    def create_internal_user(self, user_info):
-        return User.objects.create_user(user_info['name'], user_info['email'])
 
     def create_external_user(self, user_info):
         # Globus users cannot be created via their API
         pass
 
-    def create_external_user_mapping(self, user_info):
-        internal_user = User.objects.filter(username=user_info['name'], email=user_info['email']).first()
-        if not internal_user:
-            internal_user = self.create_internal_user(user_info)
-
-        external_user_mapping = {
-            'user_id': internal_user,
-            'user_authentication_name': type(self).__name__,
-            'external_user_id': user_info['sub'],
-            'external_username': user_info['name']
-        }
-
-        external_user_to_create = models.ExternalUserMapping(**external_user_mapping)
-        external_user_to_create.save()
-
-        return external_user_to_create
-
-    def internal_user_exists(self, user_id):
-        return models.ExternalUserMapping.objects.filter(
-            user_id=user_id, user_authentication_name=type(self).__name__
-        ).exists()
-
-    def external_user_exists(self, user_id):
+    def get_external_user(self, external_user_info):
         try:
-            self.oauth.get_identities(ids=user_id)
+            self.oauth.get_identities(ids=external_user_info['external_user_id'])
+            # TODO: Do additional checking here and return the User info.
         except Exception as e:
             print(e)
             return False
 
     def delete_external_user(self, user_id):
-        # Globus users cannot be created via their API
+        # Globus users cannot be deleted via their API
         pass
 
     def introspect_globus_user(self, token):
