0d782d7973524c4df9c9b1ef7db6ab0efa7acb9c
Juan Puerto <jpuerto96@hotmail.com>
2022-01-24 16:30:31 -0500

    Globus: Allow for clients to pass a globus auth token and use that for authentication.

--- user_workspaces_server/controllers/userauthenticationmethods/globus_user_authentication.py
+++ user_workspaces_server/controllers/userauthenticationmethods/globus_user_authentication.py
@@ -11,6 +11,7 @@ class GlobusUserAuthentication(AbstractUserAuthentication):
     def __init__(self, config):
         client_id = config['client_id']
         client_secret = config['client_secret']
+        self.authentication_type = config['authentication_type']
         self.oauth = globus_sdk.ConfidentialAppAuthClient(
             client_id, client_secret)
         if not AuthHelper.isInitialized():
@@ -24,61 +25,41 @@ class GlobusUserAuthentication(AbstractUserAuthentication):
     def api_authenticate(self, request):
         body = json.loads(request.body)
 
-        if 'redirect_url' not in body:
-            raise APIException('Missing redirect url.')
+        globus_user_info = self.globus_oauth_get_user_info(body) if self.authentication_type == 'oauth' \
+            else (self.globus_token_get_user_info(body) if self.authentication_type == 'token' else False)
 
-        redirect_url = body['redirect_url']
-        self.oauth.oauth2_start_flow(redirect_url)
+        if type(globus_user_info) == Response:
+            return globus_user_info
 
-        try:
-            if 'code' not in body:
-                auth_uri = self.oauth.oauth2_get_authorize_url(query_params={
-                    "scope": "openid profile email urn:globus:auth:scope:transfer.api.globus.org:all "
-                             "urn:globus:auth:scope:auth.globus.org:view_identities "
-                             "urn:globus:auth:scope:groups.api.globus.org:all"
-                })
-                return Response({'auth_uri': auth_uri})
-            else:
-                code = body['code']
-                tokens = self.oauth.oauth2_exchange_code_for_tokens(code)
+        external_user_mapping = self.get_external_user_mapping({
+            'external_user_id': globus_user_info['sub'],
+            'user_authentication_name': type(self).__name__
+        })
 
-                # Need to add call here to grab user profile info
-                user_info = self.introspect_globus_user(
-                    tokens.by_resource_server['groups.api.globus.org']['access_token'])
+        if not external_user_mapping:
+            # Since its Globus, lets get the username from the email
+            username = globus_user_info['email'].split('@')[0]
+            internal_user = self.get_internal_user({
+                'username': username,
+                'email': globus_user_info['email']
+            })
 
-                external_user_mapping = self.get_external_user_mapping({
-                    'external_user_id': user_info['sub'],
-                    'user_authentication_name': type(self).__name__
+            if not internal_user:
+                internal_user = self.create_internal_user({
+                    "username": username,
+                    "email": globus_user_info['email']
                 })
 
-                if not external_user_mapping:
-                    # Since its Globus, lets get the username from the email
-                    username = user_info['email'].split('@')[0]
-                    internal_user = self.get_internal_user({
-                        'username': username,
-                        'email': user_info['email']
-                    })
-
-                    if not internal_user:
-                        internal_user = self.create_internal_user({
-                            "username": username,
-                            "email": user_info['email']
-                        })
-
-                    user_info['internal_user_id'] = internal_user
-                    self.create_external_user_mapping({
-                        'user_id': user_info['internal_user_id'],
-                        'user_authentication_name': type(self).__name__,
-                        'external_user_id': user_info['sub'],
-                        'external_username': user_info['username']
-                    })
-                    return internal_user
-                else:
-                    return external_user_mapping.user_id
-        except Exception as e:
-            # TODO: Move print to log
-            print(e)
-            return e
+            globus_user_info['internal_user_id'] = internal_user
+            self.create_external_user_mapping({
+                'user_id': globus_user_info['internal_user_id'],
+                'user_authentication_name': type(self).__name__,
+                'external_user_id': globus_user_info['sub'],
+                'external_username': globus_user_info['username']
+            })
+            return internal_user
+        else:
+            return external_user_mapping.user_id
 
     def create_external_user(self, user_info):
         # Globus users cannot be created via their API
@@ -98,3 +79,33 @@ class GlobusUserAuthentication(AbstractUserAuthentication):
 
     def introspect_globus_user(self, token):
         return self.auth_helper.getUserInfo(token, True)
+
+    def globus_oauth_get_user_info(self, body):
+        if 'redirect_url' not in body:
+            raise APIException('Missing redirect url.')
+
+        redirect_url = body['redirect_url']
+        self.oauth.oauth2_start_flow(redirect_url)
+
+        if 'code' not in body:
+            auth_uri = self.oauth.oauth2_get_authorize_url(query_params={
+                "scope": "openid profile email urn:globus:auth:scope:transfer.api.globus.org:all "
+                         "urn:globus:auth:scope:auth.globus.org:view_identities "
+                         "urn:globus:auth:scope:groups.api.globus.org:all"
+            })
+            return Response({'auth_uri': auth_uri})
+        else:
+            code = body['code']
+            tokens = self.oauth.oauth2_exchange_code_for_tokens(code)
+
+            # Need to add call here to grab user profile info
+            return self.introspect_globus_user(
+                tokens.by_resource_server['groups.api.globus.org']['access_token'])
+
+    def globus_token_get_user_info(self, body):
+        if 'auth_token' not in body:
+            raise APIException('Missing auth_token.')
+
+        return self.introspect_globus_user(
+            body.get('auth_token'))
+
