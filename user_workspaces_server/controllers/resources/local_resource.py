84c4ffc66b93b517780f5e3535fa164346d9d2b0
Juan Puerto <jpuerto96@hotmail.com>
2022-02-04 16:43:47 -0500

    Resources: Modify local resource slightly

--- user_workspaces_server/controllers/resources/local_resource.py
+++ user_workspaces_server/controllers/resources/local_resource.py
@@ -7,17 +7,21 @@ import datetime
 
 class LocalResource(AbstractResource):
     def launch_job(self, job, workspace):
-        resource_job_id = None
-        script_name = str(workspace.id) + str(datetime.date.today()) + '.sh'
-        script_path = os.path.join(self.resource_storage.root_dir, workspace.file_path, script_name)
+        workspace_full_path = os.path.join(self.resource_storage.root_dir, workspace.file_path)
+        script_name = f"{str(datetime.datetime.now())}.sh"
+        script_path = os.path.join(workspace_full_path, script_name)
         # Get the script content
         with open(script_path, 'w') as script:
             script.write(job.script)
 
         os.chmod(script_path, 0o744)
-        process = subprocess.Popen(script_path, stdin=None, stdout=None, stderr=None)
+        process = subprocess.Popen(script_path, stdin=None, stdout=None, stderr=None, cwd=workspace_full_path)
         return process.pid
 
     def get_job(self, resource_job_id):
-        job = psutil.Process(resource_job_id)
-        return job.as_dict()
+        try:
+            job = psutil.Process(resource_job_id)
+            return job.as_dict()
+        except Exception as e:
+            print(repr(e))
+            return {'status': 'complete'}
