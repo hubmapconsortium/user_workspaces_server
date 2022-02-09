e019ef661a0d39e2f550d92ed806364ee0123008
Juan Puerto <jpuerto96@hotmail.com>
2022-02-09 13:30:05 -0500

    Resource: Use new get_script method to grab the script that needs to be written. datetime -> time

--- user_workspaces_server/controllers/resources/local_resource.py
+++ user_workspaces_server/controllers/resources/local_resource.py
@@ -2,20 +2,21 @@ from user_workspaces_server.controllers.resources.abstract_resource import Abstr
 import psutil
 import subprocess
 import os
-import datetime
+import time
 
 
 class LocalResource(AbstractResource):
     def launch_job(self, job, workspace):
         workspace_full_path = os.path.join(self.resource_storage.root_dir, workspace.file_path)
-        script_name = f"{str(datetime.datetime.now())}.sh"
+        script_name = f"{str(time.time())}.sh"
         script_path = os.path.join(workspace_full_path, script_name)
         # Get the script content
         with open(script_path, 'w') as script:
-            script.write(job.script)
+            script.write(job.get_script())
 
         os.chmod(script_path, 0o744)
-        process = subprocess.Popen(script_path, stdin=None, stdout=None, stderr=None, cwd=workspace_full_path)
+        process = subprocess.Popen(script_path,
+                                   stdin=None, stdout=None, stderr=None, cwd=workspace_full_path)
         return process.pid
 
     def get_job(self, resource_job_id):
