1ccf85bdb5d526386b9e24bea314c6be4133e196
Juan Puerto <jpuerto96@hotmail.com>
2022-02-09 13:30:22 -0500

    Job Types: Modify local test job to use get_script method.

--- user_workspaces_server/controllers/job_types/local_test_job.py
+++ user_workspaces_server/controllers/job_types/local_test_job.py
@@ -1,12 +1,19 @@
 from user_workspaces_server.controllers.job_types.abstract_job import AbstractJob
-
+import os
 
 class LocalTestJob(AbstractJob):
-    def __init__(self):
-        self.script = "#!/bin/sh\necho 'Hello World' > ./output.log\nsleep 10"
+    def __init__(self, config, job_details):
+        super().__init__(config, job_details)
+        self.script_template_name = 'local_test_template.sh'
 
     def get_script(self):
-        pass
+        with open(os.path.join(
+                os.path.abspath(os.getcwd()), 'user_workspaces_server/controllers/job_types/script_templates',
+                self.script_template_name), 'r') as f:
 
-    def status_check(self):
-        pass
+            script = f.read()
+
+        return script
+
+    def status_check(self, job_model):
+        return {}
