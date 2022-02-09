290325ccb01fb811a489ed53fd0b4c624bcfbe97
Juan Puerto <jpuerto96@hotmail.com>
2022-02-09 13:31:09 -0500

    Job Types: Update abstract job to accept params through init call

--- user_workspaces_server/controllers/job_types/abstract_job.py
+++ user_workspaces_server/controllers/job_types/abstract_job.py
@@ -2,11 +2,16 @@ from abc import ABC, abstractmethod
 
 
 class AbstractJob(ABC):
+    def __init__(self, config, job_details):
+        self.config = config
+        self.job_details = job_details
+        self.script_template_name = None
+
     @abstractmethod
     def get_script(self):
         pass
 
     @abstractmethod
-    def status_check(self):
+    def status_check(self, job_model):
         # Should return job_details information
         pass
