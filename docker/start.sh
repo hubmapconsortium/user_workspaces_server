1b2fc0552a8d0230024e3b918fe6d97827d0a27c
Juan Puerto <jpuerto96@hotmail.com>
2022-04-13 13:53:57 -0400

    General: Modify start script for it to try to connect using psql

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./docker/wait-for-it.sh -t 0 postgres:5432 -- python manage.py migrate
+bash ./docker/wait-for-postgres.sh postgres python manage.py migrate
 
 python manage.py qcluster&
 daphne -b 0.0.0.0 -p 5050 user_workspaces_server_project.asgi:application
