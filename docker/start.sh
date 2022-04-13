2d2ef0adf3e7e57355db2a52a94559bb9b7ff952
Juan Puerto <jpuerto96@hotmail.com>
2022-04-13 13:35:23 -0400

    General: Modify start.sh to use the docker directory.

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./wait-for-it.sh -t 0 postgres:5432 -- python manage.py migrate
+./docker/wait-for-it.sh -t 0 postgres:5432 -- python manage.py migrate
 
 python manage.py qcluster&
 daphne -b 0.0.0.0 -p 5050 user_workspaces_server_project.asgi:application
