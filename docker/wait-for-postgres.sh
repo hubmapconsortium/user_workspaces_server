cb3216cc51f29b50201547425ab0a12afff5578a
Juan Puerto <jpuerto96@hotmail.com>
2022-04-15 15:12:21 -0400

    General: Use env variables for the postgres connection attempt.

--- docker/wait-for-postgres.sh
+++ docker/wait-for-postgres.sh
@@ -6,7 +6,7 @@ set -e
 host="$1"
 shift
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 15
 done
