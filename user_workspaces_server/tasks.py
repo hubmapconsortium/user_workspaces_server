86ee77682adc0fac622f4e037131180f29cdd87d
Juan Puerto <jpuerto96@hotmail.com>
2022-05-23 13:26:31 -0400

    Tasks: Modify the information sent through to the websocket. Also add functionality to respect a passthrough domain (if it is set).

--- user_workspaces_server/tasks.py
+++ user_workspaces_server/tasks.py
@@ -30,13 +30,20 @@ def update_job_status(job_id):
 
     job.job_details['current_job_details'].update(job_type.status_check(job))
 
+    if job.job_details['current_job_details'].get('connection_details', {}):
+        job.job_details['current_job_details']['connection_details']['url_domain'] = \
+            resource.passthrough_domain
+
     job.save()
 
     channel_layer = get_channel_layer()
 
     async_to_sync(channel_layer.group_send)(f'job_status_{job_id}', {
         "type": "job_status_update",
-        "message": job.job_details['current_job_details']
+        "message": {
+            'status': job.status,
+            'current_job_details': job.job_details['current_job_details']
+        }
     })
 
 
