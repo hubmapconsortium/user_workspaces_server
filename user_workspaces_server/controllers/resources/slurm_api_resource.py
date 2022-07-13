efec456d243c30b8236df60f4c4b3f66dccc2de9
Juan Puerto <jpuerto96@hotmail.com>
2022-07-13 12:02:38 -0400

    Job: Remove unnecessary file creation since we just submit the script contents via the API.

--- user_workspaces_server/controllers/resources/slurm_api_resource.py
+++ user_workspaces_server/controllers/resources/slurm_api_resource.py
@@ -21,19 +21,10 @@ class SlurmAPIResource(AbstractResource):
     def launch_job(self, job, workspace):
         # Need to generate a SLURM token (as a user) to launch a job
         workspace_full_path = os.path.join(self.resource_storage.root_dir, workspace.file_path)
-        script_name = f"{str(time.time())}.sh"
-        script_path = os.path.join(workspace_full_path, script_name)
-        # Get the script content
-
-        self.resource_storage.create_file(workspace_full_path, ContentFile(bytes(job.get_script(), 'utf-8'), name=script_name))
 
         # TODO: Create an UAM for the slurm api since the token for that is different
         #  than the information for the psc user api
         user_info = self.resource_user_authentication.has_permission(workspace.user_id)
-
-        self.resource_storage.set_ownership(script_path, user_info)
-
-        os.chmod(script_path, 0o744)
 
         # For pure testing, lets just set a var in the connection details.
         headers = {'X-SLURM-USER-TOKEN': self.config.get("connection_details", {}).get("slurm_token", ""), 'X-SLURM-USER-NAME': f'{user_info.external_username}'}
