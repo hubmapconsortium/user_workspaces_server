9d5e7246d0c7e3003874d1f3dda1a810d7611563
Juan Puerto <jpuerto96@hotmail.com>
2022-07-22 16:10:59 -0400

    Jobs: Modify jupyter template to work with lmod and to check the version to make sure to use the right config structure.

--- user_workspaces_server/templates/script_templates/jupyter_lab_template.sh
+++ user_workspaces_server/templates/script_templates/jupyter_lab_template.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
-# TODO: Need to make this work with Slurm.
 # Let's assume that the module/virtualenv set-up can be moved else-where
 
-### BRIDGES2 BLOCK ###
-module load anaconda3
-### END BRIDGES2 BLOCK ###
+### Environment initialization
 
-
-# TODO: Make this configurable
-### LOCAL BLOCK ###
-#virtualenv -p python3.8 "JupyterLabJob_{{ job_id }}_venv"
-#source "JupyterLabJob_{{ job_id }}_venv/bin/activate"
-#pip install jupyterlab
-### END LOCAL BLOCK ###
+{% if module_manager=="lmod" %}
+module load {{ modules|join:" " }}
+{% elif module_manager=="virtualenv" %}
+virtualenv -p {{ python_version }} "JupyterLabJob__{{ job_id }}_venv"
+source "JupyterLabJob_{{ job_id }}_venv"
+pip install {{ pip_packages|join:" " }}
+{% endif %}
 
 # All we really need is this part
 
 CONFIG_FILE="$(pwd)/JupyterLabJob_{{ job_id }}_config.py"
 
+VERSION=$(jupyter lab --version)
+
 # Generate Jupyter configuration file with secure file permissions
 (
 umask 077
 cat > "${CONFIG_FILE}" << EOL
+if (( ${VERSION:0:1} < 3)); then
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_origin = '*'
-c.NotebookApp.root_dir = "$(pwd)"
+c.NotebookApp.notebook_dir = "$(pwd)"
 c.NotebookApp.disable_check_xsrf = True
 c.NotebookApp.base_url = "/passthrough/$(hostname)/{{ job_id }}"
-#c.ServerApp.ip = '*'
-#c.ServerApp.open_browser = False
-#c.ServerApp.allow_origin = '*'
-#c.ServerApp.root_dir = "$(pwd)"
-#c.ServerApp.disable_check_xsrf = True
-#c.ServerApp.base_url = "/passthrough/$(hostname)/{{ job_id }}"
+else
+c.ServerApp.ip = '*'
+c.ServerApp.open_browser = False
+c.ServerApp.allow_origin = '*'
+c.ServerApp.root_dir = "$(pwd)"
+c.ServerApp.disable_check_xsrf = True
+c.ServerApp.base_url = "/passthrough/$(hostname)/{{ job_id }}"
+fi
 EOL
 )
 
