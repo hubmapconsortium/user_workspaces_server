6a5a5419e7ed0af883393193f7721b44c30fcc67
Juan Puerto <jpuerto96@hotmail.com>
2022-07-27 15:00:28 -0400

    Resources: Modify local resource so that it uses a dot path for the jobs directory (under the workspaces)

--- user_workspaces_server/controllers/resources/local_resource.py
+++ user_workspaces_server/controllers/resources/local_resource.py
@@ -20,20 +20,23 @@ class LocalResource(AbstractResource):
 
     def launch_job(self, job, workspace):
         workspace_full_path = os.path.join(self.resource_storage.root_dir, workspace.file_path)
+        job_full_path = os.path.join(workspace_full_path, f'.{job.job_details["id"]}')
         script_name = f"{str(time.time())}.sh"
-        script_path = os.path.join(workspace_full_path, script_name)
+        script_path = os.path.join(job_full_path, script_name)
         # Get the script content
 
-        self.resource_storage.create_file(workspace_full_path, ContentFile(bytes(job.get_script(), 'utf-8'), name=script_name))
+        self.resource_storage.create_file(job_full_path, ContentFile(bytes(job.get_script(
+            {"workspace_full_path": workspace_full_path}), 'utf-8'), name=script_name))
 
         user_info = self.resource_user_authentication.has_permission(workspace.user_id)
 
+        self.resource_storage.set_ownership(job_full_path, user_info)
         self.resource_storage.set_ownership(script_path, user_info)
 
         os.chmod(script_path, 0o744)
 
         process = subprocess.Popen(['sudo', 'su', user_info.external_username, '-s', '/bin/bash', script_path],
-                                   stdin=None, stdout=None, stderr=None, cwd=workspace_full_path,
+                                   stdin=None, stdout=None, stderr=None, cwd=job_full_path,
                                    )
         return process.pid
 
