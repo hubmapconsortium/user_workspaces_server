89d6d8a32751866a3c6fb75e2c9cfd2a60c73ccf
Juan Puerto <jpuerto96@hotmail.com>
2022-08-10 11:49:19 -0400

    ASGI: Move url import back below otherwise we get an error.

--- user_workspaces_server_project/asgi.py
+++ user_workspaces_server_project/asgi.py
@@ -13,10 +13,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import user_workspaces_server.urls
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_workspaces_server_project.settings')
 django_asgi_app = get_asgi_application()
+
+import user_workspaces_server.urls
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
