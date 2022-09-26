187f6ba8d1bb2ea6ee79cf277b3c469e4173faea
Juan Puerto <jpuerto96@hotmail.com>
2022-09-26 14:40:33 -0400

    Auth: Add logging to auth routine.

--- user_workspaces_server/auth.py
+++ user_workspaces_server/auth.py
@@ -1,6 +1,10 @@
+import logging
+
 from rest_framework import authentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.authtoken.models import Token
+
+logger = logging.getLogger(__name__)
 
 
 class UserWorkspacesTokenAuthentication(authentication.TokenAuthentication):
@@ -10,11 +14,16 @@ class UserWorkspacesTokenAuthentication(authentication.TokenAuthentication):
         if auth_header is None:
             return None
 
-        identifier, token = auth_header.split(' ')
+        try:
+            identifier, token = auth_header.split(' ')
+        except ValueError:
+            logger.exception(f'Invalid auth header format: {auth_header}')
+            raise AuthenticationFailed('Invalid auth header format.')
 
         try:
             valid_token = Token.objects.get(key=token)
-        except Exception:
+        except Token.DoesNotExist:
+            logger.exception(f'Token {token} not found.')
             raise AuthenticationFailed('Invalid token provided.')
 
         return valid_token.user, None
