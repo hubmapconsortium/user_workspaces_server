b481d710c9e5fb2712a9ecc6769c0463ff5b1b80
Juan Puerto <jpuerto96@hotmail.com>
2022-10-04 15:27:39 -0400

    Logging: Add custom logging handler that utilizes the django q functionality to send emails out.

---
+++ user_workspaces_server/logging.py
@@ -0,0 +1,21 @@
+from logging import Handler
+
+
+class AsyncEmailHandler(Handler):
+
+    def __init__(self, from_email, email_list):
+        Handler.__init__(self)
+        self.from_email = from_email
+        self.email_list = email_list
+
+    def emit(self, record):
+        from django_q.tasks import async_task
+        msg = self.format(record)
+        email_tuple = []
+
+        for email in self.email_list:
+            email_tuple.append(('Email Subject', msg, self.from_email, [email]))
+
+        async_task('django.core.mail.send_mass_mail', tuple(email_tuple))
+
+
