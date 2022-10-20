7fdbbe37ef838f39d44d1fc844fd9b499d494423
Juan Puerto <=>
2022-10-20 16:05:14 -0400

    URLs: Add names to the URLs

--- src/user_workspaces_server/urls.py
+++ src/user_workspaces_server/urls.py
@@ -18,28 +18,28 @@ from . import views
 from . import ws_consumers
 
 token_view_patterns = [
-    path('', views.UserWorkspacesServerTokenView.as_view())
+    path('', views.UserWorkspacesServerTokenView.as_view(), name='tokens')
 ]
 
 workspace_view_patterns = [
-    path('', views.WorkspaceView.as_view()),
-    path('<int:workspace_id>/', views.WorkspaceView.as_view()),
-    path('<int:workspace_id>/<str:put_type>/', views.WorkspaceView.as_view()),
+    path('', views.WorkspaceView.as_view(), name='workspaces'),
+    path('<int:workspace_id>/', views.WorkspaceView.as_view(), name='workspaces_with_id'),
+    path('<int:workspace_id>/<str:put_type>/', views.WorkspaceView.as_view(), name='workspaces_put_type'),
 ]
 
 job_view_patterns = [
-    path('', views.JobView.as_view()),
-    path('<int:job_id>/', views.JobView.as_view()),
-    path('<int:job_id>/<str:put_type>/', views.JobView.as_view()),
+    path('', views.JobView.as_view(), name='jobs'),
+    path('<int:job_id>/', views.JobView.as_view(), name='jobs_with_id'),
+    path('<int:job_id>/<str:put_type>/', views.JobView.as_view(), name='jobs_put_type'),
 ]
 
 job_type_view_patterns = [
-    path('', views.JobTypeView.as_view()),
+    path('', views.JobTypeView.as_view(), name='job_types'),
 ]
 
 passthrough_view_patterns = [
-    path('<str:hostname>/<int:job_id>/', views.PassthroughView.as_view()),
-    path('<str:hostname>/<int:job_id>/<path:remainder>', views.PassthroughView.as_view())
+    path('<str:hostname>/<int:job_id>/', views.PassthroughView.as_view(), name='passthrough'),
+    path('<str:hostname>/<int:job_id>/<path:remainder>', views.PassthroughView.as_view(), name='passthrough_remainder')
 ]
 
 urlpatterns = [
@@ -48,11 +48,11 @@ urlpatterns = [
     path('jobs/', include(job_view_patterns)),
     path('job_types/', include(job_type_view_patterns)),
     path('passthrough/', include(passthrough_view_patterns)),
-    path('status/', views.StatusView.as_view())
+    path('status/', views.StatusView.as_view(), name='status')
 ]
 
 ws_urlpatterns = [
-    path('passthrough/<str:hostname>/<int:job_id>/<path:remainder>', ws_consumers.PassthroughConsumer.as_asgi()),
-    path('jobs/', ws_consumers.JobStatusConsumer.as_asgi()),
-    path('jobs/<int:job_id>/', ws_consumers.JobStatusConsumer.as_asgi()),
+    path('passthrough/<str:hostname>/<int:job_id>/<path:remainder>', ws_consumers.PassthroughConsumer.as_asgi(), name='ws_passthrough'),
+    path('jobs/', ws_consumers.JobStatusConsumer.as_asgi(), name='ws_jobs'),
+    path('jobs/<int:job_id>/', ws_consumers.JobStatusConsumer.as_asgi(), name='ws_job'),
 ]
