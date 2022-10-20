3ddab7c317fc615333b8428df69c35d4715cedf1
Juan Puerto <=>
2022-10-20 16:07:10 -0400

    Storage: Modify abstract class to check whether symlinks/files are a list, not a dictionary.

--- src/user_workspaces_server/controllers/storagemethods/abstract_storage.py
+++ src/user_workspaces_server/controllers/storagemethods/abstract_storage.py
@@ -10,12 +10,16 @@ class AbstractStorage(ABC):
         self.root_dir = config['root_dir']
 
     def create_symlinks(self, workspace, workspace_details):
+        if type(workspace_details.get('symlinks', [])) != list:
+            raise ParseError("'symlinks' index must contain a list.")
         for symlink in workspace_details.get('symlinks', []):
             if '..' in symlink.get('name', ''):
                 raise ParseError('Symlink name cannot contain double dots.')
             self.create_symlink(workspace.file_path, symlink)
 
     def create_files(self, workspace, workspace_details):
+        if type(workspace_details.get('files', [])) != list:
+            raise ParseError("'files' index must contain a list.")
         for file in workspace_details.get('files', []):
             # Create a file object here
             content_file = ContentFile(bytes(file.get('content', ''), 'utf-8'), name=file.get('name'))
