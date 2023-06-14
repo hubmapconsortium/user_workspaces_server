2eb22b819821a88192c76be8a026c76f225d2b91
Fatema Shormin <fshormin@psc.edu>
2023-06-14 12:06:16 -0400

    timeleftadded

--- src/user_workspaces_server/controllers/resources/slurm_api_resource.py
+++ src/user_workspaces_server/controllers/resources/slurm_api_resource.py
@@ -113,6 +113,7 @@ class SlurmAPIResource(AbstractResource):
                 raise APIException(resource_job["errors"])
 
             resource_job = resource_job["jobs"][0]
+            
             resource_job["status"] = self.translate_status(resource_job["job_state"])
             return resource_job
         except Exception as e:
