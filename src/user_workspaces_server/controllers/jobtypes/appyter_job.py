3013873c13413590f7d7915581e05d17c6129915
Juan Puerto <=>
2024-03-01 14:41:47 -0500

    General: Basic support for appyter to test this out.

---
+++ src/user_workspaces_server/controllers/jobtypes/appyter_job.py
@@ -0,0 +1,100 @@
+import logging
+import os
+from datetime import datetime
+from urllib import parse
+
+from django.apps import apps
+from django.template import loader
+
+from user_workspaces_server import models
+from user_workspaces_server.controllers.jobtypes.abstract_job import AbstractJob
+
+logger = logging.getLogger(__name__)
+
+
+class AppyterJob(AbstractJob):
+    def __init__(self, config, job_details):
+        super().__init__(config, job_details)
+        self.script_template_name = "appyter_template.sh"
+        # Check to see whether job_details contains a notebook path
+
+    def get_script(self, template_params=None):
+        template_config = {"job_id": self.job_details["id"]}
+        template_config.update(self.config)
+        template_config.update(template_params)
+
+        template = loader.get_template(f"script_templates/{self.script_template_name}")
+        script = template.render(template_config)
+
+        return script
+
+    def status_check(self, job_model):
+        output_file_name = f"AppyterJob_{job_model.id}_output.log"
+        resource = apps.get_app_config("user_workspaces_server").main_resource
+
+        if job_model.status == models.Job.Status.FAILED:
+            return {
+                "message": "This job has failed. Support team has been notified and will investigate the error."
+            }
+
+        # Check to see if we already have a connection url in place.
+        if "connection_details" in job_model.job_details["current_job_details"]:
+            return {}
+
+        try:
+            with open(
+                os.path.join(
+                    resource.resource_storage.root_dir,
+                    job_model.workspace_id.file_path,
+                    f".{job_model.id}",
+                    output_file_name,
+                )
+            ) as f:
+                log_file = f.readlines()
+        except FileNotFoundError:
+            logger.warning(
+                f"Appyter output file {job_model.workspace_id.file_path}/.{job_model.id} missing."
+            )
+            return {"current_job_details": {"message": "Webserver not ready."}}
+
+        url = ""
+
+        for line in log_file:
+            if "http" in line:
+                url = parse.urlparse(line.split("] ")[1])
+                break
+
+        if not url:
+            return {"current_job_details": {"message": "No url found."}}
+
+        port = url.port
+        hostname = url.hostname
+
+        try:
+            token = parse.parse_qs(url.query)["token"][0].strip()
+        except KeyError:
+            logger.warning("Token missing in Appyter output.")
+            return {"current_job_details": {"message": "Token undefined."}}
+
+        connection_string = f"{url.path}?token={token}"
+
+        time_init = (
+            datetime.now(job_model.datetime_start.tzinfo) - job_model.datetime_start
+        ).total_seconds()
+
+        return {
+            "metrics": {
+                "time_init": time_init,
+            },
+            "current_job_details": {
+                "message": "Webserver ready.",
+                "proxy_details": {
+                    "hostname": hostname,
+                    "port": port,
+                    "path": connection_string,
+                },
+                "connection_details": {
+                    "url_path": connection_string,
+                },
+            },
+        }
---
+++ src/user_workspaces_server/templates/script_templates/appyter_template.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+[[ {{ environment_name }} == .* ]] && ENV_NAME="{{ environment_name }}" || ENV_NAME=".{{ environment_name }}"
+VENV_PATH="{{ workspace_full_path }}/${ENV_NAME}_venv"
+
+echo "STARTED @ $(date)"
+### Environment initialization
+{% if module_manager == "tar" %}
+  if [ ! -d "$VENV_PATH" ]; then
+    mkdir -p "$VENV_PATH"
+    tar -xf {{ tar_file_path }} -C "$VENV_PATH"
+    echo "VENV COPIED @ $(date)"
+    source "$VENV_PATH/bin/activate"
+    conda-unpack
+    echo "VENV UNPACKED @ $(date)"
+    source "$VENV_PATH/bin/deactivate"
+  fi
+  source "$VENV_PATH/bin/activate"
+{% endif %}
+{% if module_manager == "lmod" %}
+  module load {{ modules|join:" " }}
+  {% if use_local_environment %}
+    if [ ! -d "$VENV_PATH" ]; then
+      export PYTHONNOUSERSITE=True
+      conda create --prefix "$VENV_PATH" python={{ python_version }} -y
+    fi
+    source activate "$VENV_PATH"
+    pip install {{ python_packages|join:" " }}
+  {% endif %}
+{% elif module_manager == "virtualenv" %}
+  if [ ! -d "$VENV_PATH" ]; then
+    virtualenv -p {{ python_version }} "$VENV_PATH"
+  fi
+  source "$VENV_PATH/bin/activate"
+  pip install {{ python_packages|join:" " }}
+{% endif %}
+
+random_number () {
+    shuf -i ${1}-${2} -n 1
+}
+
+port_used_python() {
+  python -c "import socket; socket.socket().connect(('$1',$2))" >/dev/null 2>&1
+}
+
+port_used_python3() {
+  python3 -c "import socket; socket.socket().connect(('$1',$2))" >/dev/null 2>&1
+}
+
+port_used_nc(){
+  nc -w 2 "$1" "$2" < /dev/null > /dev/null 2>&1
+}
+
+port_used_lsof(){
+  lsof -i :"$2" >/dev/null 2>&1
+}
+
+port_used_bash(){
+  local bash_supported=$(strings /bin/bash 2>/dev/null | grep tcp)
+  if [ "$bash_supported" == "/dev/tcp/*/*" ]; then
+    (: < /dev/tcp/$1/$2) >/dev/null 2>&1
+  else
+    return 127
+  fi
+}
+
+# Check if port $1 is in use
+port_used () {
+  local port="${1#*:}"
+  local host=$((expr "${1}" : '\(.*\):' || echo "localhost") | awk 'END{print $NF}')
+  local port_strategies=(port_used_nc port_used_lsof port_used_bash port_used_python port_used_python3)
+
+  for strategy in ${port_strategies[@]};
+  do
+    $strategy $host $port
+    status=$?
+    if [[ "$status" == "0" ]] || [[ "$status" == "1" ]]; then
+      return $status
+    fi
+  done
+
+  return 127
+}
+
+# Find available port in range [$2..$3] for host $1
+# Default: [2000..65535]
+find_port () {
+  local host="${1:-localhost}"
+  local port=$(random_number "${2:-2000}" "${3:-65535}")
+  while port_used "${host}:${port}"; do
+    port=$(random_number "${2:-2000}" "${3:-65535}")
+  done
+  echo "${port}"
+}
+
+PORT=$(find_port)
+
+# Launch the Jupyter Notebook Server
+set -x
+python -m appyter --port $(PORT) --proxy true --host $(uname -n) &> "$(pwd)/AppyterJob_{{ job_id }}_output.log"
