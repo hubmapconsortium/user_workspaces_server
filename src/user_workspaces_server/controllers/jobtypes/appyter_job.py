220178039d9418469b4d35fcf3124701192a398b
Juan Puerto <=>
2024-03-04 15:13:13 -0500

    Appyter: Add support for passing a notebook_path

--- src/user_workspaces_server/controllers/jobtypes/appyter_job.py
+++ src/user_workspaces_server/controllers/jobtypes/appyter_job.py
@@ -16,10 +16,15 @@ class AppyterJob(AbstractJob):
     def __init__(self, config, job_details):
         super().__init__(config, job_details)
         self.script_template_name = "appyter_template.sh"
-        # Check to see whether job_details contains a notebook path
+        self.notebook_path = self.job_details["job_details"]["request_job_details"].get(
+            "notebook_path", "appyter.ipynb"
+        )
 
     def get_script(self, template_params=None):
-        template_config = {"job_id": self.job_details["id"]}
+        template_config = {"job_id": self.job_details["id"], "notebook_path": self.notebook_path}
+
+        logger.info(self.notebook_path)
+
         template_config.update(self.config)
         template_config.update(template_params)
 
