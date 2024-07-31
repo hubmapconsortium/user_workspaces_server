993dd52b5ec17cd0d5bcd4d1f2588813ac457610
Juan Puerto <=>
2024-07-31 14:58:06 -0400

    General: Reformatting

--- src/user_workspaces_server/controllers/resources/slurm_api_resource.py
+++ src/user_workspaces_server/controllers/resources/slurm_api_resource.py
@@ -90,9 +90,12 @@ class SlurmAPIResource(AbstractResource):
         )
 
         if slurm_response.status_code != 200:
-            raise APIException(slurm_response.text if slurm_response.text
-                               else "No error message returned from Slurm API, please contact "
-                                    "system administrator for more information.")
+            raise APIException(
+                slurm_response.text
+                if slurm_response.text
+                else "No error message returned from Slurm API, please contact "
+                "system administrator for more information."
+            )
 
         try:
             slurm_response = slurm_response.json()
