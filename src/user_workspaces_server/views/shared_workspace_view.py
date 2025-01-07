0a2df19c1f643f3d16bbbf112ceeeaa508b72eae
Juan Puerto <=>
2025-01-07 13:09:36 -0500

    Views: Update SharedWorkspaceView GET request.

--- src/user_workspaces_server/views/shared_workspace_view.py
+++ src/user_workspaces_server/views/shared_workspace_view.py
@@ -13,25 +13,44 @@ class SharedWorkspaceView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, shared_workspace_id=None):
-        shared_workspace = models.SharedWorkspaceMapping.objects.filter(
+        # For a given user we need to return:
+        # - The shared workspaces for which they've sent requests
+        # - The shared workspaces for which they've received requests
+        # We need to filter based on whether they've been accepted or not
+        # We need to allow for a single shared workspace to be returned
+
+        original_workspaces = models.SharedWorkspace.objects.filter(
+            original_workspace_id__user_id=request.user
+        )
+
+        shared_workspaces = models.SharedWorkspaceMapping.objects.filter(
             shared_workspace_id__user_id=request.user
         )
 
         if shared_workspace_id:
-            shared_workspace = shared_workspace.filter(id=shared_workspace_id)
+            shared_workspaces = models.SharedWorkspaceMapping.objects.filter(
+                id=shared_workspace_id
+            )
         elif params := request.GET:
-            query_param_fields = models.SharedWorkspaceMapping.get_query_param_fields()
-            for key in set(params.keys()).intersection(query_param_fields.keys()):
-                query_param_field = query_param_fields[key]
-                shared_workspace = shared_workspace.filter(**{query_param_field: params[key]})
+            for key in set(params.keys()).intersection(
+                models.SharedWorkspaceMapping.get_query_param_fields()
+            ):
+                original_workspaces = original_workspaces.filter(**{key: params[key]})
+                shared_workspaces = shared_workspaces.filter(**{key: params[key]})
 
-        shared_workspaces = list(
-            shared_workspace.all().values(*models.SharedWorkspaceMapping.get_dict_fields())
-        )
+        dict_fields = models.SharedWorkspaceMapping.get_dict_fields()
 
-        response = {"message": "Successful.", "success": True, "data": {"shared_workspaces": []}}
+        original_workspaces = list(original_workspaces.all().values(*dict_fields))
+        shared_workspaces = list(shared_workspaces.all().values(*dict_fields))
 
-        if shared_workspaces:
+        response = {
+            "message": "Successful.",
+            "success": True,
+            "data": {"sent": [], "received": []},
+        }
+
+        if original_workspaces or shared_workspaces:
+            response["data"]["original_workspaces"] = original_workspaces
             response["data"]["shared_workspaces"] = shared_workspaces
         else:
             response["message"] = "Shared workspace matching given parameters could not be found."
