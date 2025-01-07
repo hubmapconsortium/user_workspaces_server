0f3c563b2bab2b7a7dbfa0cf7ee4d63fd96ae83e
Juan Puerto <=>
2025-01-07 15:00:01 -0500

    Models: Make fields dynamic for workspaces serializer

--- src/user_workspaces_server/models.py
+++ src/user_workspaces_server/models.py
@@ -150,13 +150,15 @@ class WorkspaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workspace
         fields = Workspace.get_dict_fields()
+        fields.append("user_id")
 
     def __init__(self, *args, **kwargs):
-        workspace_type = kwargs.pop("workspace_type")
+        workspace_type = kwargs.pop("workspace_type", None)
         super().__init__(*args, **kwargs)
 
         if workspace_type == "shared_workspace":
-            self.fields = ["id", "user_id", "name", "description"]
+            for field_name in set(self.fields) - {"id", "user_id", "name", "description"}:
+                self.fields.pop(field_name)
 
 
 class SharedWorkspaceMappingSerializer(serializers.ModelSerializer):
