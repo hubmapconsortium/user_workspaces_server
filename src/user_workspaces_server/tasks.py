dbbf6328283bb93b05ce9e5a3319c6250f78d9be
Juan Puerto <=>
2025-01-23 11:15:37 -0500

    Tasks: Tuple != ()

--- src/user_workspaces_server/tasks.py
+++ src/user_workspaces_server/tasks.py
@@ -333,7 +333,7 @@ def initialize_shared_workspace(shared_workspace_mapping_id: int):
     )
     async_task(
         "django.core.mail.send_mail",
-        tuple("Workspaces Sharing", message, None, [shared_workspace.user_id.email]),
+        ("Workspaces Sharing", message, None, [shared_workspace.user_id.email]),
     )
 
     shared_workspace.save()
