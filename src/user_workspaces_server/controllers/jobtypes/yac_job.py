6a31ea9d89a0404aa67bd2ac9e5ffc55b04a4e64
Juan Puerto <=>
2025-11-25 14:24:12 -0500

    General: Add check to make sure web server is actually ready.

--- src/user_workspaces_server/controllers/jobtypes/yac_job.py
+++ src/user_workspaces_server/controllers/jobtypes/yac_job.py
@@ -90,7 +90,7 @@ class YACJob(AbstractJob):
         )
 
         if http_r.get(url_domain).status_code != 200:
-            logger.warning(f"Webserver not ready yet.")
+            logger.warning("Webserver not ready yet.")
             return {"current_job_details": {"message": "Webserver not ready."}}
 
         return {
